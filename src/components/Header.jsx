acb067f0ba0a7ae787a0116f87f1c767e4753346
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-10-31 19:12:39 -0500

    add header logo & footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import driven_results_logo2 from '../images/driven_results_logo 2.svg'
+import './css/Header.css'
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <>
+        <div id="header">
+            <img src={driven_results_logo2} alt="driven_logo" className="logo" />
+            <div>(337) 123-4567</div>
         </div>
+        </>
     )
 }
 
--- src/components/NavDrop.jsx
+++ src/components/NavDrop.jsx
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom'
 
 const NavDrop = () => {
     return (
-        <>
+      <>
         <div id="navBar">
           <Nav.Item>
             <Nav.Link><Link to="/" className="fa-home">Home</Link></Nav.Link>
            </Nav.Item>
           <Nav.Item>
+              <Nav.Link><Link to="/services" className="fa-services">Services</Link></Nav.Link>
+           </Nav.Item>
+          <Nav.Item>
             <Nav.Link><Link to="/about" className="fa-about">About</Link></Nav.Link>
            </Nav.Item>
-           <Nav.Item>
+          <Nav.Item>
             <Nav.Link><Link to ="/contact" className="fa-contact">Contact</Link></Nav.Link>
            </Nav.Item>
-           <div className="headIconsSpace">
-           </div>
          </div>
        </>
     )
