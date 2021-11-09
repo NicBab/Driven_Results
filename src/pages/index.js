c001130e21e0af575be7d524765982f0782d7701
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-09 07:04:41 -0600

    update/add footer routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,11 @@
+/* Nav drop select */
 export { default as About } from './About'
 export { default as Contact } from './Contact'
 export { default as Home } from './Home' 
 export { default as Services } from './Services'
+
+/*Footer select*/
+export { default as Policies } from './Policies'
+/*Help is link to ContactPage*/
+export { default as History } from './History'
+export { default as Blog } from './Blog'
