057c583b7674c1141dede265ddc9c56e7a910531
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-11 11:24:57 -0600

    clean up footer and add footer social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import IconsHeader from './IconsHeader'
 import './css/Footer.css'
 
 const Footer = () => {
     return (
-        <Container className="d-flex align-items-end justify-content-around mt-10" 
-            style={{minHeight: "10vh"}}>
-              <>
-            <div>
-            <footer className="site-footer">
-              <section className="footer-links">
-                <h2>Customer Care</h2>
-                <a href="">Policies</a>
-                <a href="">Help</a>
-              </section>
-              <br/>
-              <section className="footer-links">
-                <h2>Sales</h2>
-                <a href="">Advertise</a>
-              </section>
-              <section className="footer-links">
-                <h2 id>About Us</h2>
-                <a href="">Blog</a>
-                <a href="">Our History</a>
-                <a href="">Take a Tour</a>
-              </section>
-            </footer>
+        <>
+          <div>
+            <div className="footerIcons">
+              <IconsHeader/>
             </div>
-          </>
-      </Container>
+            <footer className="site-footer">
+            <section className="footer-links">
+              <h2>Customer Care</h2>
+              <Link to="/policies">Policies</Link>
+              <Link to="/contact">Help</Link>
+            </section>
+            <section className="footer-links">
+              <h2 id>About Us</h2>
+                <Link to="/history">History</Link>
+                <Link to="/blog">Blog</Link>
+            </section>
+          </footer>
+        </div>
+      </>
     )
 }
 
